870cacd17fc9bd9c9bf15d20563e224b6f50a813
maxarvid <max.arvid.anderson@gmail.com>
2022-06-12 13:18:26 +0200

    OOS add type check to morning page model spec

--- spec/models/morning_page_spec.rb
+++ spec/models/morning_page_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe MorningPage, type: :model do
   describe 'DB Table' do
-    it { is_expected.to have_db_column :title }
-    it { is_expected.to have_db_column :body }
+    it { is_expected.to have_db_column(:title).of_type(:string) }
+    it { is_expected.to have_db_column(:body).of_type(:text) }
   end
 
   describe 'Validations' do
