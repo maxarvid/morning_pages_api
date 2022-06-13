a15d5822f2d97f3cb3ae93a48201da09df589362
maxarvid <max.arvid.anderson@gmail.com>
2022-06-13 10:24:15 +0200

    🥅 moves error handling to application controller

--- spec/requests/morning_pages/create_spec.rb
+++ spec/requests/morning_pages/create_spec.rb
@@ -1,11 +1,13 @@
-describe 'POST /morning_pages', type: :request do
+describe 'POST themes/:id/morning_pages', type: :request do
+  subject { response }
+  let(:theme) { create(:theme) }
   let(:user) { create(:user) }
   let(:credentials) { user.create_new_auth_token }
 
   describe 'successfully' do
     before do
-      post '/morning_pages', params: { morning_page: { title: 'My awesome title', body: 'My thoughtful body' } },
-                             headers: credentials
+      post "/themes/#{theme.id}/morning_pages", params: { morning_page: { title: 'My awesome title', body: 'My thoughtful body' } },
+                                                headers: credentials
     end
 
     it { is_expected.to have_http_status 201 }
@@ -13,6 +15,37 @@ describe 'POST /morning_pages', type: :request do
     it 'is expected to respond with a message' do
       expect(response_json['message']).to eq 'The page was saved to the database'
     end
-    
+
+    it 'is expected to respond with the body of the morning page' do
+      expect(response_json['morning_page']['body']).to eq 'My thoughtful body'
+    end
+  end
+
+  describe 'unsuccessfully' do
+    describe 'because of missing credentials' do
+      before do
+        post "/themes/#{theme.id}/morning_pages", params: { morning_page: { title: 'My awesome title', body: 'My thoughtful body' } },
+                                                  headers: nil
+      end
+
+      it { is_expected.to have_http_status 401 }
+
+      it 'is expected to respond with an error' do
+        expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
+      end
+    end
+
+    describe 'because of missing body' do
+      before do
+        post "/themes/#{theme.id}/morning_pages", params: { morning_page: { title: 'My awesome title', body: nil } },
+                                                  headers: credentials
+      end
+
+      it { is_expected.to have_http_status 422 }
+
+      it 'is expected to respond with an error' do
+        expect(response_json['errors'].first).to eq "Body can't be blank"
+      end
+    end
   end
 end
