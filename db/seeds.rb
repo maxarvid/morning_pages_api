f88c4cea4875421db789bde0271a27ca4cad62cc
maxarvid <max.arvid.anderson@gmail.com>
2022-06-15 14:11:02 +0200

    🌱 adds db seed for the 12 themes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Themes.create([{ name: 'A Sense of Safety' },
+               { name: 'A Sense of Identity' },
+               { name: 'A Sense of Power' },
+               { name: 'A Sense of Integrity' },
+               { name: 'A Sense of Possibility' },
+               { name: 'A Sense of Abundance' },
+               { name: 'A Sense of Connection' },
+               { name: 'A Sense of Stength' },
+               { name: 'A Sense of Compassion' },
+               { name: 'A Sense of Self-Protection' },
+               { name: 'A Sense of Autonomy' },
+               { name: 'A Sense of Faith' }])
