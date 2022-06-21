be4eedbd6cf0fd1c18a4cbd5d0cd9533d42774d2
maxarvid <max.arvid.anderson@gmail.com>
2022-06-21 12:29:14 +0200

    🧪 adds failing spec to see that only morning pages belonging to user are returned

--- spec/requests/morning_pages/show_spec.rb
+++ spec/requests/morning_pages/show_spec.rb
@@ -1,7 +1,9 @@
 describe 'GET /morning_pages/:id', type: :request do
   let(:user) { create(:user) }
   let(:credentials) { user.create_new_auth_token }
-  let(:morning_page) { create(:morning_page) }
+  let(:morning_page) { create(:morning_page, user: user) }
+  let!(:other_morning_page) { create(:morning_page) }
+
   subject { response }
 
   describe 'when user is authenticated' do
@@ -11,8 +13,20 @@ describe 'GET /morning_pages/:id', type: :request do
 
     it { is_expected.to have_http_status 200 }
 
-    it 'is expected to return the body of the morning page' do
+    it 'is expected to return the body of the morning page belonging to the user' do
       expect(response_json['morning_page']['body']).to eq morning_page.body
+    end
+  end
+
+  describe 'when user is authenicated but morning page belongs to another user' do
+    before do
+      get "/morning_pages/#{other_morning_page.id}", headers: credentials
+    end
+
+    it { is_expected.to have_http_status 401 }
+
+    it 'is expected to return an error message' do
+      expect(response_json['errors'].first).to eq 'some message'
     end
   end
 
